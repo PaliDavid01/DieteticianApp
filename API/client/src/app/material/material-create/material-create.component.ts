b25220d6bc91fcc621e6ea5844eeee418d63a0ce
Páli Dávid <pali.david2001@outlook.com>
2024-04-24 07:51:00 +0200

    basematerial allergen in create frontend is working, backend needed

--- API/client/src/app/material/material-create/material-create.component.ts
+++ API/client/src/app/material/material-create/material-create.component.ts
@@ -1,30 +1,99 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Data, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import {
+  Allergen,
+  AllergenMaterial,
+  AllergenMaterialService,
+  AllergenService,
   BaseMaterial,
   BaseMaterialService,
+  MaterialGroup,
+  MaterialGroupService,
 } from 'src/app/services/generated-client';
 
 @Component({
   selector: 'app-material-create',
   templateUrl: './material-create.component.html',
   styleUrls: ['./material-create.component.css'],
+  providers: [
+    BaseMaterialService,
+    MaterialGroupService,
+    AllergenMaterialService,
+  ],
 })
 export class MaterialCreateComponent implements OnInit {
   displayDialog: boolean = true;
-  saveMaterial() {
-    console.log(this.material);
-  }
+  materialGroups: MaterialGroup[] = [];
+  selectedAllergens: Allergen[] = [];
+  allergens: Allergen[] = [];
   material: BaseMaterial;
   submitted: boolean;
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private baseMaterialService: BaseMaterialService
+    private baseMaterialService: BaseMaterialService,
+    private materialGroupService: MaterialGroupService,
+    private messageService: MessageService,
+    private allergenMaterialService: AllergenMaterialService,
+    private allergenService: AllergenService
   ) {}
+
   ngOnInit(): void {
     this.route.data.subscribe((data) => {
       this.material = data['material'];
     });
+    this.materialGroupService.readAll().subscribe((data) => {
+      this.materialGroups = data;
+    });
+    this.allergenService.readAll().subscribe((data) => {
+      this.allergens = data;
+    });
+    if (this.material.materialId) {
+      console.log(this.material.materialId);
+      this.allergenMaterialService
+        .getAllergensByMaterialId(this.material.materialId)
+        .subscribe((data) => {
+          this.selectedAllergens = this.allergens.filter((allergen) =>
+            data.some(
+              (allergenMaterial) =>
+                allergenMaterial.allergenCode === allergen.allergenCode
+            )
+          );
+        });
+    }
+  }
+
+  saveMaterial() {
+    if (this.material.materialId) {
+      this.baseMaterialService.update(this.material).subscribe(
+        (data) => {
+          this.router.navigate(['/material']);
+        },
+        (error) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: error.error,
+            life: 3000,
+          });
+        }
+      );
+    } else {
+      this.baseMaterialService.create(this.material).subscribe(
+        (data) => {
+          this.router.navigate(['/material']);
+        },
+        (error) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: error.error,
+            life: 3000,
+          });
+        }
+      );
+    }
   }
 }
